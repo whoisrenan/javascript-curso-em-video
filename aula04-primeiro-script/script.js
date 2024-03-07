d57c2cd226d7850a3ee43adf15f6083cdfcb80a5
Renan Lemos <renanlemos1612@gmail.com>
2024-03-07 18:03:07 -0300

    aula-variaveis-e-tipos-de-dados

--- aula04-primeiro-script/script.js
+++ aula04-primeiro-script/script.js
@@ -1,3 +1,6 @@
 alert("Minha primeira mensagem")
+// Alert dispara um alerta no navegador.
 confirm("Está gostando de JavaScript?")
+//  Confirm pede confirmação do usuário, uma janela com Ok e Cancel.
 prompt("Qual é o seu nome?")
+//  prompt abre uma caixa de resposta para o usuário.
