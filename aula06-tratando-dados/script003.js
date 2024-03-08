6033cb8284e40dbe4cc0e0026d13d8223bc30be2
Renan Lemos <renanlemos1612@gmail.com>
2024-03-08 17:17:59 -0300

    respondendo-atividades

--- aula06-tratando-dados/script003.js
+++ aula06-tratando-dados/script003.js
@@ -1,4 +1,28 @@
-var nome = prompt("Qual é o seu nome?")
-document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras <br>`)
-document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br>`)
-document.write(`Seu nome em minúsculas é ${nome.toLowerCase()} <br>`)
+var nome = prompt("Qual é o seu nome?");
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras <br>`);
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br>`);
+document.write(`Seu nome em minúsculas é ${nome.toLowerCase()} <br>`);
+// ___________________________________________________________________
+//                         Atividades Práticas:
+// Q001: Como é possível guardar o valor digitado em um prompt() dentro de uma variável.
+// RESPOSTA: var valor = prompt("Digite qualquer coisa: ")
+// document.write(`O valor digitado foi ${valor}`)
+// ___________________________________________________________________
+// Q002: Como fazer com que um número digitado em um prompt() possa ser usado em cálculos.
+// RESPOSTA: var n1 = Number(prompt("Digite um número: "))
+// var n2 = Number(prompt("Digite outro número: "))
+// var soma = n1 + n2
+// document.write(`o número digitado foi ${n1} e ${n2}. A soma entre eles é ${soma} `)
+// ___________________________________________________________________
+// Q003: Como transformar um texto todo para letras maiúsculas?
+// RESPOSTA: var texto = prompt("Digite qualquer coisa para que seu texto seja transformado em letras maiúsculas: ")
+// document.write(`O texto em maiúsculas: ${texto.toUpperCase()}`)
+// ___________________________________________________________________
+// Q004: Como mostrar um número formatado como um valor monetário.
+// RESPOSTA: var salario = Number(prompt("Digite o valor do seu sálario: "));
+// document.write(
+//   `O valor do seu salário é ${salario.toLocaleString("pt-BR", {
+//     style: "currency",
+//     currency: "BRL",
+//   })} reais!`
+// );
