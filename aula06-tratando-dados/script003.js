4f2f9829fb0e338715fb108f162f88643e28177c
Renan Lemos <renanlemos1612@gmail.com>
2024-03-10 19:25:50 -0300

    resolvendo atividades operadores

--- aula06-tratando-dados/script003.js
+++ aula06-tratando-dados/script003.js
@@ -27,7 +27,4 @@ document.write(`Seu nome em minúsculas é ${nome.toLowerCase()} <br>`);
 //   })} reais!`
 // );
 //  __________________________________________________________________
-// Q005: Use o operador ternário para colocar um entre dois valores em uma variável
-// var idade = 17;
-// var adulto = idade>= 18? "Adulto" : "Adolescente"
-// console.log(adulto)
+
---
+++ aula07-operadores/script.js
@@ -0,0 +1,4 @@
+// Q005: Use o operador ternário para colocar um entre dois valores em uma variável
+var idade = 17;
+var adulto = idade>= 18? "Adulto" : "Adolescente"
+console.log(adulto)
