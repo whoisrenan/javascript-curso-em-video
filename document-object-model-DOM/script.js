86d1975f408337b1987e8c378775c94ebff523e3
Renan Lemos <renanlemos1612@gmail.com>
2024-03-10 20:17:04 -0300

    intro ao document object model

---
+++ document-object-model-DOM/script.js
@@ -0,0 +1,9 @@
+var corpo = document.body
+var p1 = document.getElementsByTagName('p')[1]
+// document.getElementsByTagName seleciona a primeira tag mostrada no document html ('p'), pois foi especificado entre chaves [0]. 
+// var d = document.getElementById('msg')
+// d.style.background = 'green'
+// d.innerText = 'Estou aguardando'
+
+var d = document.querySelector('#msg')
+d.style.background ='blue'
